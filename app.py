23cf685f05a06365982e017c2655b9200d81024c
chidichekwas <79763339+chidichekwas@users.noreply.github.com>
2025-03-26 16:26:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ load_dotenv()
 config = AutoConfig()
 data_directory = os.path.join(os.path.dirname(__file__), "data")
 
+OPENAI_API_KEY = "sk-OsMMq65tXdfOIlTUYtocSL7NCsmA7CerN77OkEv29dODg1EA"
+GPT_MODEL="gpt-4o-mini"
 
 # Initialize session states
 if 'history' not in st.session_state:
